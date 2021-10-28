c1c41046b5b2b86fe11400b2b6889ee196455bce
Diego Di Giorgio <diegofdg@gmail.com>
2021-10-28 16:05:55 -0300

    terminando la funcionalidad del menu en JS

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,14 @@
 (function(){
-    const openButton = document.querySelector('.nav__menu');    
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');    
 
     openButton.addEventListener('click', ()=>{
-        console.log("hiciste click");        
+        menu.classList.add('nav__link--show');
     });
+
+    closeMenu.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
 })();
